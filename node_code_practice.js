38110f983860895fc9df3616eca85ee3545e6133
salomon <salomon.cloud.dev@gmail.com>
2024-06-23 15:47:55 -0400

    node

--- node_code_practice.js
+++ node_code_practice.js
@@ -1,6 +1,6 @@
 /* first project in node.js */
-const mission = 'get a cloud job';
-if (mission === 'get a cloud job') {
+const mission = 'getting a cloud job';
+if (mission === 'getting a cloud job') {
     console.log('you will need programming skills!');
 } else {
     console.log(`Is ${mission} a challenge?`);
