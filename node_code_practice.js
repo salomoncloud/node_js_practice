034b042e37cdc07fe7d6f01177ed860e67bbdcce
salomon <salomon.cloud.dev@gmail.com>
2024-06-23 15:45:32 -0400

    node

--- node_code_practice.js
+++ node_code_practice.js
@@ -1,6 +1,7 @@
-/* testing */
-function findMeaning() {
-    return 42;
+/* first project in node.js */
+const mission = 'get a cloud job';
+if (mission === 'get a cloud job') {
+    console.log('you will need programming skills!');
+} else {
+    console.log(`Is ${mission} a challenge?`);
 }
-
-console.log(findMeaning());
